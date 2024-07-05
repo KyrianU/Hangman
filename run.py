482b4e24ead6b48ddea2c8cf945bde9c28d0f70c
Kyrian Uwaegbulem <kyrian.uwaegbulem@gmail.com>
2024-07-05 14:07:03 +0000

    added structure to the get_word_category function

--- run.py
+++ run.py
@@ -24,6 +24,27 @@ def hangman_title():
 
 hangman_title()
 
+def get_word_category():
+    """
+    Select a category from the following 3
+    categories
+    """
+    print("\n Please choose between the following options: \n")
+    print("  ---- ANIMALS ---- CARS ---- COUNTRIES ---- \n")
+    user_choice = input("").lower()
+
+    print(f"\n You chose {user_choice}, Good luck! \n")
+
+    if user_choice == "cars":
+        return car_brands, "cars"
+    elif user_choice == "animals":
+        return animal_list, "animals"
+    elif user_choice == "countries":
+        return country_list, "countries"
+    else:
+        print(f"This option is not valid! Please try again")
+    return get_word_category()
+
 def get_random_words(secret_word):
     """
     Generate random words from the
@@ -79,6 +100,7 @@ def rules():
         All the best!
         """
     )
+    print('\n')
     menu()
 
 
@@ -98,3 +120,8 @@ def play_game():
     print(secret_word)
     
 
+if __name__== "__main__":
+    """
+    Start Game
+    """
+    menu()
